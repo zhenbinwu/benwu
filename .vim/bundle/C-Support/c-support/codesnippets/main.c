940871429c38d294e0df1eb6f44cb3af2493a13e
Ben Wu <zhenbinwu@gmail.com>
2013-10-23 11:23:35 -0500

    Minor update

--- .vim/bundle/C-Support/c-support/codesnippets/main.c
+++ .vim/bundle/C-Support/c-support/codesnippets/main.c
@@ -10,11 +10,10 @@
  *  Description:  main function
  * =====================================================================================
  */
-	int
-main ( int argc, char *argv[] )
+int main ( int argc, char *argv[] )
 {
-	printf ("\nProgram %s\n\n", argv[0] );
+  printf ("\nProgram %s\n\n", argv[0] );
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }		/* ----------  end of function main  ---------- */
 
