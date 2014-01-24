8e68d9adb22b7290830089218b7a2676cf322f85
Ben@nbay04 <benwu@fnal.gov>
2014-01-24 17:25:48 -0600

    Add new alias tb to open root file

---
+++ .tcsh/newBrowser.C
@@ -0,0 +1,20 @@
+// ===========================================================================
+// 
+//       Filename:  newBrowser.C
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  01/24/2014 01:42:53 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Zhenbin Wu (benwu), benwu@fnal.gov
+//        Company:  Baylor University, CDF@FNAL, CMS@LPC
+// 
+// ===========================================================================
+
+void newBrowser()
+{
+  new TBrowser;
+}
